4006d4e9977147e6c4d3d1b4b0c81acff02f2e51
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-09-01 09:56:00 +0400

    Task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого
+// элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 50);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число строк (m)");
+if(!int.TryParse(Console.ReadLine()!, out var m)) {
+    Console.WriteLine("Всё плохо");
+}
+
+Console.WriteLine("Введите число столбцов (n)");
+if(!int.TryParse(Console.ReadLine()!, out var n)) {
+    Console.WriteLine("Всё плохо");
+}
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string Position(int[,] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (num == array[m, n]) result += $"({m}, {n})";
+        }
+    }
+    if (result == string.Empty) result = "Такого элемента нет";
+    return result;
+}
+Console.WriteLine(Position(array));
